ce4d7b3dcf6f80e9caab65dabab62006acc3ff51
Rebbox <37891786+RemmosLost@users.noreply.github.com>
2023-09-16 00:02:45 -0600

    Bullet
    Disparos dirigidos con mause

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,29 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bullet : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
-
-    private Rigidbody2D MyRb;
-    public float Speed;
-
+    public float Speed = 10f;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        MyRb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        transform.right = mousePos - transform.position; // Orienta la bala hacia la posición del mouse.
+        rb.velocity = transform.right * Speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MyRb.velocity = transform.right * Speed;
-        Destroy(gameObject, 7f);
+        // Agrega cualquier lógica adicional aquí si es necesario.
     }
 
     // Detectar colisiones
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       Destroy(gameObject);
+        Destroy(gameObject);
     }
 }
+
