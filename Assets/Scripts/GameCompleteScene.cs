3acb61f512d706dbfc9eeb2aef5ac2adbbee04b3
Jur15 <moralesjur@gmail.com>
2023-09-18 12:29:23 -0600

    Bugfix: Destroy bullet after enemy hit
    +Quit Unity Editor in end screen

--- Assets/Scripts/GameCompleteScene.cs
+++ Assets/Scripts/GameCompleteScene.cs
@@ -13,5 +13,8 @@ public class GameCompleteScene : MonoBehaviour
   public void QuitGame()
   {
     Application.Quit();
+    #if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false; //Sale del modo Play en el editor de Unity
+    #endif
   }
 }
